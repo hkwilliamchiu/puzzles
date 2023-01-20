4576d245e3a8d83e2487c875484543dde4cc0709
hkwilliamchiu <hkchiu@uchicago.edu>
2023-01-20 16:27:24 +0000

    Complete leap.py

--- leap/leap.py
+++ leap/leap.py
@@ -1,7 +1,10 @@
 def leap_year(year):
-    if (year % 4) == 0:
-        if (year % 100) == 0:
-            if (year % 400) == 0:
-            return True
-            else
-            return False
+    if (year % 400 == 0) and (year % 100 == 0):
+        return True
+    elif (year % 4 ==0) and (year % 100 != 0):
+        return True
+    else:
+        return False
+
+
+        
