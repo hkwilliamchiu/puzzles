10726b5bdab8aba7b5e260ebd602b03bf622532a
hkwilliamchiu <hkchiu@uchicago.edu>
2023-01-20 16:58:47 +0000

    Complete collatz-conjecture.py

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,15 @@
 def steps(number):
-    pass
+    step = 0
+    if (number < 1):
+        raise ValueError("Sorry, no numbers below 1")
+    elif (number == 1):
+        return (step)
+    else:
+        while (number > 1):
+            if number % 2 == 0:
+                number = number/2
+            else:
+                number = number*3 + 1
+            
+            step = step + 1
+        return (step)
